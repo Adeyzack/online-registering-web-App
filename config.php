e87ced90217aa8031d2f9ca2bc0d944b32abdc82
Adeyzack <95370312+Adeyzack@users.noreply.github.com>
2024-06-20 23:51:06 -0400

    update config header
    changed db login information
    
    Co-Authored-By: AD2024-source <172735678+ad2024-source@users.noreply.github.com>

--- config.php
+++ config.php
@@ -1,8 +1,13 @@
 <?php
-$host = 'localhost';
-$db = 'student_registration';
-$user = 'zack';
-$pass = 'password'; // Default MySQL password for XAMPP is empty
+//$host = 'localhost';
+//$db = 'student_registration';
+//$user = 'zack';
+//$pass = 'password'; // Default MySQL password for XAMPP is empty
+
+$host = 'us-cluster-east-01.k8s.cleardb.net';
+$db = 'heroku_76effe95fc8ce84';
+$user = 'bb5dc6dd6a0ac1';
+$pass = 'b7a5f0dc'; // Default MySQL password for XAMPP is empty
 
 $conn = new mysqli($host, $user, $pass, $db);
 
